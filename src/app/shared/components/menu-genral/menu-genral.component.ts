7800e46aa9cf2682533ce73548f2cd6f7b6bdb44
Chaima <chaima.akremi.1997@gmail.com>
2023-04-25 18:47:23 +0200

    add bootstrap menu

--- src/app/shared/components/menu-genral/menu-genral.component.ts
+++ src/app/shared/components/menu-genral/menu-genral.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-menu-genral',
   templateUrl: './menu-genral.component.html',
   styleUrls: ['./menu-genral.component.css']
 })
-export class MenuGenralComponent {
+export class MenuGenralComponent implements OnInit{
+ @ViewChild('zoneRecherche', {static: false})
+ maZoneDeRecherche: ElementRef | undefined;
+ @Output()
+ lancerRecherche: EventEmitter<string> = new EventEmitter<string>()
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
+  rechercher(){
+    this.lancerRecherche.emit(this.maZoneDeRecherche?.nativeElement.value);
+  }
 
 }
